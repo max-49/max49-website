d1ec6733039fc01f2c5c5e9ac1677466abb14051
max49 <massimomarino49@gmail.com>
2021-09-06 22:11:57 -0400

    js fix

--- static/js/writeups.js
+++ static/js/writeups.js
@@ -14,7 +14,7 @@ function changeFrameInfo() {
 }
 
 function changeFrame9() {
-  document.getElementById("channel-title").innerHTML = "<a href=\"/round-9-writeups.html\" target=\"_blank\" style=\"color: white;\">round-9-writeups</a>";
+  document.getElementById("channel-title").innerHTML = "<a href=\"/round-9-writeups\" target=\"_blank\" style=\"color: white;\">round-9-writeups</a>";
   document.getElementById("server-info").style.display = "none";
   document.getElementById("round-9-writeups").style.display = "block";
   document.getElementById("round-10-writeups").style.display = "none";
@@ -25,7 +25,7 @@ function changeFrame9() {
 }
 
 function changeFrame10() {
-  document.getElementById("channel-title").innerHTML = "<a href=\"/round-10-writeups.html\" target=\"_blank\" style=\"color: white;\">round-10-writeups</a>";
+  document.getElementById("channel-title").innerHTML = "<a href=\"/round-10-writeups\" target=\"_blank\" style=\"color: white;\">round-10-writeups</a>";
   document.getElementById("server-info").style.display = "none";
   document.getElementById("round-9-writeups").style.display = "none";
   document.getElementById("round-10-writeups").style.display = "block";
@@ -36,7 +36,7 @@ function changeFrame10() {
 }
 
 function changeFrame11() {
-  document.getElementById("channel-title").innerHTML = "<a href=\"/round-11-writeups.html\" target=\"_blank\" style=\"color: white;\">round-11-writeups</a>";
+  document.getElementById("channel-title").innerHTML = "<a href=\"/round-11-writeups\" target=\"_blank\" style=\"color: white;\">round-11-writeups</a>";
   document.getElementById("server-info").style.display = "none";
   document.getElementById("round-9-writeups").style.display = "none";
   document.getElementById("round-10-writeups").style.display = "none";
@@ -47,7 +47,7 @@ function changeFrame11() {
 }
 
 function changeFrame12() {
-  document.getElementById("channel-title").innerHTML = "<a href=\"/round-12-writeups.html\" target=\"_blank\" style=\"color: white;\">round-12-writeups</a>";
+  document.getElementById("channel-title").innerHTML = "<a href=\"/round-12-writeups\" target=\"_blank\" style=\"color: white;\">round-12-writeups</a>";
   document.getElementById("server-info").style.display = "none";
   document.getElementById("round-9-writeups").style.display = "none";
   document.getElementById("round-10-writeups").style.display = "none";
@@ -58,7 +58,7 @@ function changeFrame12() {
 }
 
 function changeFrame13() {
-  document.getElementById("channel-title").innerHTML = "<a href=\"/round-13-writeups.html\" target=\"_blank\" style=\"color: white;\">round-13-writeups</a>";
+  document.getElementById("channel-title").innerHTML = "<a href=\"/round-13-writeups\" target=\"_blank\" style=\"color: white;\">round-13-writeups</a>";
   document.getElementById("server-info").style.display = "none";
   document.getElementById("round-9-writeups").style.display = "none";
   document.getElementById("round-10-writeups").style.display = "none";
@@ -69,7 +69,7 @@ function changeFrame13() {
 }
 
 function changeFrame14() {
-  document.getElementById("channel-title").innerHTML = "<a href=\"/round-14-writeups.html\" target=\"_blank\" style=\"color: white;\">round-14-writeups</a>";
+  document.getElementById("channel-title").innerHTML = "<a href=\"/round-14-writeups\" target=\"_blank\" style=\"color: white;\">round-14-writeups</a>";
   document.getElementById("server-info").style.display = "none";
   document.getElementById("round-9-writeups").style.display = "none";
   document.getElementById("round-10-writeups").style.display = "none";

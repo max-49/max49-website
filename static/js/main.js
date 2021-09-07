5a0ba19de6612c9dc206072154c770347512ae5e
max-49 <47071544+max-49@users.noreply.github.com>
2021-09-06 20:03:25 -0400

    Add files via upload

---
+++ static/js/main.js
@@ -0,0 +1,58 @@
+function changeImage() {
+  if (document.getElementById("imgClickAndChange").src == "https://www.jing.fm/clipimg/full/132-1327227_moon-clipart-moon-clipart-black-and-white-half.png") 
+  {
+    document.getElementById("imgClickAndChange").src = "https://www.jing.fm/clipimg/full/63-631481_rays-clipart-black-grey-sun-sun-cartoon-png.png";
+    document.body.style.background = "#ffffff";
+    document.body.style.color = "#37393e";
+    var x = document.getElementsByClassName("chall");
+    for (var i = 0; i < x.length; i++) {
+        x[i].style.background = "#f3f3f4";
+        x[i].style.color = "#37393e"
+    }
+    var x = document.getElementsByClassName("pre");
+    for (var i = 0; i < x.length; i++) {
+        x[i].style.background = "#f3f3f4";
+        x[i].style.color = "#37393e";
+    }
+    var oldlink = document.getElementsByTagName("link").item(0);
+
+    var newlink = document.createElement("link");
+    newlink.setAttribute("rel", "stylesheet");
+    newlink.setAttribute("type", "text/css");
+    newlink.setAttribute("href", "https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@10.7.2/build/styles/default.min.css");
+
+    document.getElementsByTagName("head").item(0).replaceChild(newlink, oldlink);
+  }
+  else 
+  { 
+    document.getElementById("imgClickAndChange").src = "https://www.jing.fm/clipimg/full/132-1327227_moon-clipart-moon-clipart-black-and-white-half.png";
+    document.body.style.background = "#37393e";
+    document.body.style.color = "#ffffff";
+    var x = document.getElementsByClassName("chall");
+    for (var i = 0; i < x.length; i++) {
+        x[i].style.background = "#2f3037";
+        x[i].style.color = "#ffffff";
+    }
+    var x = document.getElementsByClassName("pre");
+    for (var i = 0; i < x.length; i++) {
+        x[i].style.background = "#1e1e1e";
+        x[i].style.color = "#ffffff";
+    }
+    var oldlink = document.getElementsByTagName("link").item(0);
+
+    var newlink = document.createElement("link");
+    newlink.setAttribute("rel", "stylesheet");
+    newlink.setAttribute("type", "text/css");
+    newlink.setAttribute("href", "https://cdn.jsdelivr.net/gh/highlightjs/cdn-release@10.7.2/build/styles/an-old-hope.min.css");
+
+    document.getElementsByTagName("head").item(0).replaceChild(newlink, oldlink);
+  }
+}
+
+function showCode() {
+  if (document.getElementById("icicle-beginner").style.display == "inline") {
+    document.getElementById("icicle-beginner").style.display = "none";
+  } else {
+    document.getElementById("icicle-beginner").style.display = "inline";
+  }
+}
---
+++ static/js/writeups.js
@@ -0,0 +1,63 @@
+window.onload = function() {
+  changeFrameInfo();
+};
+
+function changeFrameInfo() {
+  document.getElementById("channel-title").innerHTML = "server-info";
+  document.getElementById("server-info").style.display = "block";
+  document.getElementById("round-9-writeups").style.display = "none";
+  document.getElementById("round-10-writeups").style.display = "none";
+  document.getElementById("round-11-writeups").style.display = "none";
+  document.getElementById("round-12-writeups").style.display = "none";
+  document.getElementById("round-13-writeups").style.display = "none";
+}
+
+function changeFrame9() {
+  document.getElementById("channel-title").innerHTML = "<a href=\"/round-9-writeups.html\" target=\"_blank\" style=\"color: white;\">round-9-writeups</a>";
+  document.getElementById("server-info").style.display = "none";
+  document.getElementById("round-9-writeups").style.display = "block";
+  document.getElementById("round-10-writeups").style.display = "none";
+  document.getElementById("round-11-writeups").style.display = "none";
+  document.getElementById("round-12-writeups").style.display = "none";
+  document.getElementById("round-13-writeups").style.display = "none";
+}
+
+function changeFrame10() {
+  document.getElementById("channel-title").innerHTML = "<a href=\"/round-10-writeups.html\" target=\"_blank\" style=\"color: white;\">round-10-writeups</a>";
+  document.getElementById("server-info").style.display = "none";
+  document.getElementById("round-9-writeups").style.display = "none";
+  document.getElementById("round-10-writeups").style.display = "block";
+  document.getElementById("round-11-writeups").style.display = "none";
+  document.getElementById("round-12-writeups").style.display = "none";
+  document.getElementById("round-13-writeups").style.display = "none";
+}
+
+function changeFrame11() {
+  document.getElementById("channel-title").innerHTML = "<a href=\"/round-11-writeups.html\" target=\"_blank\" style=\"color: white;\">round-11-writeups</a>";
+  document.getElementById("server-info").style.display = "none";
+  document.getElementById("round-9-writeups").style.display = "none";
+  document.getElementById("round-10-writeups").style.display = "none";
+  document.getElementById("round-11-writeups").style.display = "block";
+  document.getElementById("round-12-writeups").style.display = "none";
+  document.getElementById("round-13-writeups").style.display = "none";
+}
+
+function changeFrame12() {
+  document.getElementById("channel-title").innerHTML = "<a href=\"/round-12-writeups.html\" target=\"_blank\" style=\"color: white;\">round-12-writeups</a>";
+  document.getElementById("server-info").style.display = "none";
+  document.getElementById("round-9-writeups").style.display = "none";
+  document.getElementById("round-10-writeups").style.display = "none";
+  document.getElementById("round-11-writeups").style.display = "none";
+  document.getElementById("round-12-writeups").style.display = "block";
+  document.getElementById("round-13-writeups").style.display = "none";
+}
+
+function changeFrame13() {
+  document.getElementById("channel-title").innerHTML = "<a href=\"/round-12-writeups.html\" target=\"_blank\" style=\"color: white;\">round-13-writeups</a>";
+  document.getElementById("server-info").style.display = "none";
+  document.getElementById("round-9-writeups").style.display = "none";
+  document.getElementById("round-10-writeups").style.display = "none";
+  document.getElementById("round-11-writeups").style.display = "none";
+  document.getElementById("round-12-writeups").style.display = "none";
+  document.getElementById("round-13-writeups").style.display = "block";
+}
